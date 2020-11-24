1224297d6db0b7e0bea8d18f2eb8ac1ed9b01951
Leandro  Bergamo <leandro.bergamo@hotmail.com>
2020-11-24 18:45:15 -0300

    adicionando o process

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import './App.css';
-const socket = io('http://localhost:3333', {
+const socket = io(process.env.WEBSOCKET_CONNECT ?? 'http://localhost:3333', {
 	withCredentials: true,
 	extraHeaders: {
 		"my-custom-header": "abcd"
 	}
 });
 function App() {
+	const [status, setStatus] = useState('');
+	const [count, setCount] = useState();
+	const user = new Object();
+	user.localization = 16;
+	user.service = 15;
+	user.user = 1;
 
-	const [code, setCode] = useState(0);
-	// const [status, setStatus] = useState('');
-	// const [resposta, setResposta] = useState('');
-	
+	// exit() {
+	// 	socket.emit('exit', user)
+	// };
+
 	useEffect(() => {
-		// socket.on('code', (code) => {
-		// 	setCode(code)
-		// });
-		socket.on('code', (code) => {
-			socket.emit('user', code);
+		socket.on('status', (code) => {
+			setStatus(code);
 		});
-		socket.emit('message', "to chegando")
+		socket.on('queue', (count) => {
+			setCount(count);
+		});
+		socket.emit('entryQueue', user)
 	}, []);
 	return (
 		<div>
-			<h2>{ code }</h2>
-			{/* <h2>{ resposta }</h2>
-			<h2>{ status }</h2> */}
+			{/* <h2>{ code }</h2> */}
+			{<h2>{ status }</h2> }	
+			{<h2>{ count }</h2> }
+			<button onclick={() => function(){ this.socket.emit('exit');}}> opa </button>
 		</div>
 	);
 }
